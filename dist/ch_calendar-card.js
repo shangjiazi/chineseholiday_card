8e95f04678f29b1eff06049c85ea4cf5f101c6f2
shangjiazi <64829367+shangjiazi@users.noreply.github.com>
2023-12-05 16:11:45 +0800

    Update ch_calendar-card.js

--- dist/ch_calendar-card.js
+++ dist/ch_calendar-card.js
@@ -170,17 +170,13 @@ class ChineseCalendarCard extends LitElement {
           <div class="date_lunar">
             ${this.attributes.lunar}
           </div>          
-
-	  
           <div class="date_week">
-            <p class="icon_state" style="background: none, url(${this.getStateIcon(this.calendarEntity.state)}) no-repeat; background-size: contain;"></p>
             ${this.attributes.week}
+            <p class="icon_state" style="background: none, url(${this.getStateIcon(this.calendarEntity.state)}) no-repeat; background-size: contain;"></p>   
           </div>
-
           <div class="date_shji">
             ${this.currentTime}
           </div>
-        
           <div class="latest_title">距离</div>
           <div class="latest_holiday">${this.latestReminder.name}</div>
           <div class="flex_container_center">
